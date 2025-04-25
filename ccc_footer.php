4f4d1b332791a1a48a040bde231c24007cc3e1ef
Jocelynn Torres <jocelynn.torres@hunterbusinessschool.edu>
2025-04-25 10:30:42 -0400

    Initial Commit

---
+++ ccc_footer.php
@@ -0,0 +1,17 @@
+<!-- Chapter 2, Exercise 2-5 Coast City Computers Header
+	Date: 4/22/2025
+	Author: Jocelynn T. -->
+
+<hr/>
+<table width="100%" style="border: 0;">
+	<tr>
+		<td><strong>Updated:</strong>April 22, 2025</td>
+		<td style="text-align: right;">
+			<?php
+				$year = date("Y");
+				echo "&copy;$year by Coast City Computers";
+			?>
+		</td>
+	</tr>
+
+</table>
---
+++ ccc_header.php
@@ -0,0 +1,11 @@
+<!-- Chapter 2, Exercise 2-5 Coast City Computers Header
+	Date: 4/22/2025
+	Author: Jocelynn T. -->
+
+<table width="100%" style="border: 0;">
+	<tr>
+		<td><h1 style="font-family: 'Georgia';">Coast City Computers</h1></td>
+		<td><h3>Buy Online or Call: 1-800-555-1212</h3></td>
+	</tr>
+</table>
+<hr/>
